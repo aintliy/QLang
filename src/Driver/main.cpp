6b3c8cdb6bc105851fd0bed0a06981de03792563
aintliy <andrewleefcn@gmail.com>
2026-04-11 16:09:08 +0800

    feat(semantic): wire Checker into Driver compilation pipeline

--- src/Driver/main.cpp
+++ src/Driver/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include "Lexer.h"
+#include "Parser.h"
+#include "Checker.h"
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -18,14 +20,16 @@ int main(int argc, char** argv) {
     std::string source((std::istreambuf_iterator<char>(file)),
                         std::istreambuf_iterator<char>());
 
+    // Lex
     Lexer lexer(source);
-    while (true) {
-        Token token = lexer.nextToken();
-        std::cout << "Token: " << tokenKindToString(token.kind)
-                  << " \"" << token.lexeme << "\""
-                  << " at " << token.line << ":" << token.col << std::endl;
-        if (token.kind == TokenKind::END_OF_FILE) break;
-    }
+
+    // Parse
+    Parser parser(lexer);
+    auto ast = parser.parseProgram();
+
+    // Semantic check
+    Checker checker(std::move(ast));
+    checker.check();  // Errors will be printed by Diagnostics::error
 
     return 0;
 }
